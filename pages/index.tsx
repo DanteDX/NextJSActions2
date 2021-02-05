7d0fe3e8226f2c23efbe558ca57f0bc79778460b
dantedxwolf <teacher.martin@gmail.com>
2021-02-05 06:50:06 +0600

    tailwindcss added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import House from "./house/index";
 export default function Home() {
   return (
     <div data-testid="mainWrapper">
       Working
+      <House />
     </div>
   )
 }
